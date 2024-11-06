9f7e797771e8cc1e82b3942e56291844a3d8a723
Sai Vanga <ToSaiVanga@gmail.com>
2024-11-05 17:30:13 -0800

    Updated fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,13 @@
 let fibo = [0, 1];
 
-num = 10;
+num = -30;
 
-while (fibo.length <= num - 1) {
-  fibo.push(fibo[fibo.length - 2] + fibo[fibo.length - 1]);
+if (num = 1) {
+  fibo.pop();
+} else {
+  while (fibo.length <= num - 1) {
+    fibo.push(fibo[fibo.length - 2] + fibo[fibo.length - 1]);
+  }
 }
 
 console.log(fibo);
