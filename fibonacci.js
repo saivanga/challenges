eb034165a091d747f6d450535334190981f66522
Sai Vanga <ToSaiVanga@gmail.com>
2024-11-05 17:23:02 -0800

    Updated fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,7 @@ let fibo = [0, 1];
 num = 10;
 
 while (fibo.length <= num - 1) {
-  fibo.push(fibo.length - 2 + fibo.length - 1);
+  fibo.push(fibo[fibo.length - 2] + fibo[fibo.length - 1]);
 }
 
 console.log(fibo);
